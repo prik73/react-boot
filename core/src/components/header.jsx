5f0d5ff6d8947f210284f4c199a6dbe489195843
Saarthak1234 <saarthakrishideep@gmail.com>
2024-07-21 15:40:36 +0530

    added a logo image, and changed the css (orange to blue)

--- core/src/components/header.jsx
+++ core/src/components/header.jsx
@@ -1,24 +1,26 @@
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import imglogo from "../assets/images/site/logo.jpg"; // Assuming your logo is a static image
 
-
-export default function AppHeader(){
-    return(
-        <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home">saarthak</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="/about-us">about us</Nav.Link>
-              <Nav.Link href="/gallery">gallery</Nav.Link>
-              <Nav.Link href="/link">contact-us</Nav.Link>
-           
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    )
+export default function AppHeader() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src={imglogo} alt="Your Company Logo" className="navbar-logo"/>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="/about-us">About Us</Nav.Link>
+            <Nav.Link href="/gallery">Gallery</Nav.Link>
+            <Nav.Link href="/contact-us">Contact Us</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
