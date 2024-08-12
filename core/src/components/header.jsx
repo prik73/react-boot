a42d290a71d927e692c0394015650867810cb5c8
Priyanshu <prinovac@gmail.com>
2024-08-12 22:34:30 +0530

    Update header.jsx

--- core/src/components/header.jsx
+++ core/src/components/header.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import imglogo from "../assets/images/site/logo.jpg"; // Assuming your logo is a static image
+import imglogo from "../assets/images/site/logo.jpg";
 
 export default function AppHeader() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">
-          <a href='/home'>
+        <Navbar.Brand as={Link} to="/home">
           <img src={imglogo} alt="Your Company Logo" className="navbar-logo"/>
-          </a>
-          
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/home">Home</Nav.Link>
-            <Nav.Link href="/about-us">About Us</Nav.Link>
-            <Nav.Link href="/gallery">Gallery</Nav.Link>
-            <Nav.Link href="/contact-us">Contact Us</Nav.Link>
+            <Nav.Link as={Link} to="/home">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about-us">About Us</Nav.Link>
+            <Nav.Link as={Link} to="/gallery">Gallery</Nav.Link>
+            <Nav.Link as={Link} to="/contact-us">Contact Us</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
