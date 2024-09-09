b4595945c7bba0f6cb897defb6caa7e5af8b1d21
Saarthak1234 <141902426+Saarthak1234@users.noreply.github.com>
2024-09-09 08:04:11 +0530

    Changes

--- core/src/pages/Donate.jsx
+++ core/src/pages/Donate.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import './donateForm.css';
-
+import QR from '../assets/images/site/DonateUs.jpg'
 const Donate = () => {
   return (
     <div className="donate-us-container">
       <Container fluid>
-        <Row className="justify-content-center">
-          <Col xs={12} md={6} className="mb-4 mb-md-0">
-            <h2>Help Children by Donating to Us</h2>
-            <p>You can donate by either bank transfer or using the QR code below.</p>
+        <Row style={{}} className="justify-content-center">
+          <Col style={{borderRadius: "20px", background: 'linear-gradient(325deg, rgba(255,255,255,0.1) -20%, rgba(0,0,255,0.8) 75%)', backgroundcolor: "#1d8aff"}} xs={12} md={6} className="mb-4 mb-md-0">
+            <h3 style={{ textAlign: 'center', color: "white" }}>Every donation makes a difference.</h3>
+            <p style={{ textAlign: 'center', color: "white" }}>You can donate by either bank transfer or using the given QR code.</p>
             <div className="bank-details">
-              <h4>Bank Transfer Details:</h4>
-              <p>Bank Name: XYZ Bank</p>
-              <p>Account Number: 1234567890</p>
-              <p>IFSC Code: XYZ1234</p>
+              <h4 style={{ textAlign: 'center', marginTop:'45px', color: "white" }}>Bank Transfer Details:</h4>
+              <p style={{ textAlign: 'center' , color: "white"}}>Bank Name: IDBI Bank</p>
+              <p style={{ textAlign: 'center' , color: "white"}}>Account Number: 2252102000000082</p>
+              <p style={{ textAlign: 'center' , color: "white"}}>IFSC Code: IBKL0002252</p>
             </div>
           </Col>
           <Col xs={12} md={6} className="qr-code-section">
             <h4>Scan QR Code to Donate</h4>
-            <img src="path/to/qr-code.png" alt="QR Code" className="qr-code" />
+            <img src={QR} alt="QR Code" className="qr-code" />
           </Col>
         </Row>
       </Container>
