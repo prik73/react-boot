f2261d4312b28fe837937e640fd72bcf82e53c0c
prik73 <prinovac@gmail.com>
2024-09-30 00:59:00 +0530

    about-gallery made responsive

--- core/src/components/AboutGallery.jsx
+++ core/src/components/AboutGallery.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import './AboutGallery.css';
 import i13 from '../assets/images/imgs/img13.jpg'
 import i2 from '../assets/images/imgs/img2.jpg'
 import i9 from '../assets/images/imgs/img9.jpg'
@@ -65,77 +66,33 @@ export default function AboutGallery() {
     }, [datas]);
 
     return (
-        <div>
+        <div className="about-gallery-container">
             {/* Modal */}
             {datas.img && (
-                <div style={{
-                    marginTop: '100px',
-                    width: '100%',
-                    height: '100vh',
-                    background: 'rgba(2, 2, 2, 0.75)',
-                    position: 'fixed',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    overflow: 'hidden',
-                    top: '0',
-                    left: '0',
-                    zIndex: 1000,
-                    backdropFilter: 'blur(20px)',
-                    borderRadius: '10px',
-                    border: '1px solid rgba(255, 255, 255, 0.3)'
-                }}>
-                    <button onClick={closeModal} style={{
-                        position: 'absolute',
-                        top: '60px',
-                        right: '20px',
-                        background: 'transparent',
-                        color: 'white',
-                        border: 'none',
-                        fontSize: '24px',
-                        cursor: 'pointer',
-                        overflow: 'hidden',
-                    }}>X</button>
-                    <button onClick={() => imgAction('prev-img')} style={{
-                        position: 'absolute',
-                        left: '350px',
-                        background: 'transparent',
-                        color: 'white',
-                        border: 'none',
-                        fontSize: '36px',
-                        cursor: 'pointer'
-                    }}>&lt;</button>
-                    <img src={datas.img} style={{ width: 'auto', maxWidth: '90%', maxHeight: '90%' }} alt="Modal content" />
-                    <button onClick={() => imgAction('next-img')} style={{
-                        position: 'absolute',
-                        right: '350px',
-                        background: 'transparent',
-                        color: 'white',
-                        border: 'none',
-                        fontSize: '36px',
-                        cursor: 'pointer'
-                    }}>&gt;</button>
-                </div>
+                <div className="about-gallery-modal">
+                <button className="about-gallery-close-btn" onClick={closeModal}>X</button>
+                <button className="about-gallery-prev-btn" onClick={() => imgAction('prev-img')}>&lt;</button>
+                <img src={datas.img} alt="Modal content" />
+                <button className="about-gallery-next-btn" onClick={() => imgAction('next-img')}>&gt;</button>
+            </div>
             )}
 
             {/* Gallery */}
-            <div style={{ padding: '100px' }}>
-                <ResponsiveMasonry
-                    columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
-                >
-                    <Masonry gutter="20px">
-                        {data.map((item, index) => (
-                            <img
-                                key={index}
-                                src={item.imgSrc}
-                                style={{ width: '100%', display: 'block', cursor: 'pointer' }}
-                                alt={`Gallery Image ${index + 1}`}
-                                onClick={() => viewImage(item.imgSrc, index)}
-                            />
-                        ))}
-                    </Masonry>
-                </ResponsiveMasonry>
-            </div>
+            <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
+                <Masonry gutter="20px">
+                    {data.map((item, index) => (
+                        <img
+                            key={index}
+                            src={item.imgSrc}
+                            className="gallery-img"
+                            style={{ width: '100%', display: 'block', cursor: 'pointer' }}
+                            alt={`Gallery Image ${index + 1}`}
+                            onClick={() => viewImage(item.imgSrc, index)}
+                        />
+                    ))}
+                </Masonry>
+            </ResponsiveMasonry>
         </div>
     );
+
 }
